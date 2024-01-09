02a5063a8cbd4bf57cc741b7c30cf8129490f632
Muhammad Olamide <muhammadolammi@gmail.com>
2024-01-09 09:50:58 +0100

    create server file

--- main.go
+++ main.go
@@ -1,45 +1,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println(err)
-	}
-	port := os.Getenv("PORT")
-	// Define CORS options
-	corsOptions := cors.Options{
-		AllowedOrigins:   []string{"*"}, // You can customize this based on your needs
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"}, // You can customize this based on your needs
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum age for cache, in seconds
-	}
-	router := chi.NewRouter()
-	v1 := chi.NewRouter()
-	router.Use(cors.Handler(corsOptions))
-	v1.Get("/hello", func(w http.ResponseWriter, r *http.Request) { respondWithJson(w, 200, "hello") })
-	v1.Get("/error", func(w http.ResponseWriter, r *http.Request) { respondWithError(w, 200, "this is an error test") })
-	router.Mount("/v1", v1)
-	srv := &http.Server{
-		Addr:              ":" + port,
-		Handler:           router,
-		ReadHeaderTimeout: time.Minute,
-	}
-	log.Printf("serving server on port %v", port)
-
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
+	server()
 
 }
